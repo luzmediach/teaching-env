986fc93694ebcc52e5bb83511463705385819b60
Marc Chehab <marc@luzmedia.ch>
2023-03-02 11:09:48 +0100

    jupyter

---
+++ jupyter-notebooks/textadventure/descriptions.py
@@ -0,0 +1,15 @@
+title =""" 
+0=====================================0
+|  Flucht aus der Höhle des Terrors!  |
+0=====================================0
+"""
+
+intro = """ 
+Hier beginnt das Abenteuer:
+Sie wachen in einer schwach beleuchteten Höhle mit starken Kopfschmerzen auf. 
+Bla bla bla,
+"""
+
+
+if __name__ == "__main__":
+    main()
